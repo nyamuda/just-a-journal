12d5e2ad25d3a5fccc98f25a6b4ebc0327000339
Pierce Nyamuda <ptnrlab@gmail.com>
2022-10-19 12:18:25 +0200

    shelljs added to generate a swagger.json file

--- src/index.ts
+++ src/index.ts
@@ -3,9 +3,11 @@ import * as dotenv from "dotenv";
 import { connect_database } from "./models/index";
 import * as routes from "./routes/index";
 import cors from "cors";
+import shell from "shelljs";
 dotenv.config();
 
-
+//generate the swagger.json file
+shell.exec("npm run swagger");
 
 
 let app = express();
@@ -23,6 +25,7 @@ app.use(routes.oauthRoutes);
 app.use(routes.loginRoutes);
 app.use(routes.registerRoutes);
 app.use(routes.swaggerRoutes);
+
 
 
 
